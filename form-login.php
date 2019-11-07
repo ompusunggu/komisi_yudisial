0a9da6b274b4ad63d74a5e98f548b55d3fbbbf98
Louis Dwysevrey Ompusunggu <louis.ompusungu@gdn-commerce.com>
2019-11-07 22:44:10 +0700

    add new file

---
+++ form-login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Formulir LOGIN Siswa Baru | SMK Coding</title>
+</head>
+
+<body>
+<header>
+  <h3>Formulir LOGIN Siswa Baru</h3>
+</header>
+
+<form action="proses-login.php" method="POST">
+
+  <fieldset>
+
+    <p>
+      <label for="username">Email : </label>
+      <input type="text" name="username" placeholder="email@mail.com"/>
+    </p>
+
+    <p>
+      <label for="password">Password: </label>
+      <input type="password" name="password"/>
+    </p>
+    <p>
+      <input type="submit" value="Login" name="login"/>
+    </p>
+
+  </fieldset>
+
+</form>
+
+</body>
+</html>
+
