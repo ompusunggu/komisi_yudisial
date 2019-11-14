bb06a05bed914d50aec7b2bb5b30a0349b2289a9
Louis Dwysevrey Ompusunggu <louis.ompusungu@gdn-commerce.com>
2019-11-14 20:13:29 +0700

    add config php

---
+++ html/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$server = "localhost";
+$user = "yudisial";
+$password = "yudisial";
+$nama_database = "komisi_yudisial";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
+?>
+
