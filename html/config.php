181ad56d0e158b2706ae7cd12f5d1dbe3aa80c74
Louis Dwysevrey Ompusunggu <louis.ompusungu@gdn-commerce.com>
2019-11-19 22:04:22 +0700

    fix db config

--- html/config.php
+++ html/config.php
@@ -5,7 +5,9 @@ $user = "yudisial";
 $password = "yudisial";
 $nama_database = "komisi_yudisial";
 
-$db = mysqli_connect($server, $user, $password, $nama_database);
+if($db == null){
+    $db = mysqli_connect($server, $user, $password, $nama_database);
+}
 
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
